72f50ee0e11ed6afe4e5af8a28b0b5c891965057
Liam Ellis <liamhellis@gmail.com>
2018-01-17 17:41:50 -0500

    node env

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -22,6 +22,7 @@ export default class Home extends Component {
     render() {
         const square = { width: 175, height: 175, color: 'white' }
         console.log(process.env.STAGING)
+        console.log(process.env.NODE_ENV)
         
 
         return (
