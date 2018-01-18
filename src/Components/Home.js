42dd4d6d06ea9f127f16d08100a3e5ffe0622e1f
Liam Ellis <liamhellis@gmail.com>
2018-01-18 01:24:35 -0500

    css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -51,19 +51,24 @@ export default class Home extends Component {
                 <div className="slideshow" id="splash">
                     <div className="slide">
                         <div className="slide__bg slide__bg--6"></div>
-                        <h1 className="word word--6">Block Party</h1>
-                        
-                            <div id="signUpMenu">
-                                <h3>The first Music Streaming Community on the Block</h3>
-                                <Header as='h2' icon>
-                                    <Icon name='music' />
-                                    Sign up as an
-                                        <Header.Subheader color='white'>
-                                        <Link to='/artists/signup'>Artists</Link>
-                                    </Header.Subheader>
-                                </Header>
+                        <h1 className="word word--6">BLOCK PARTY</h1>
 
-                                <Header as='h2' icon>
+                        <div id="signUpMenu">
+                            <h3>The first Music Streaming Community on the Block</h3>
+                            <p>With the advent of Blockchain technology we've created a platform where every second you listen to your favorite artist you're mining on the block. Here's what's incredible: It pays you and your artist.</p>
+                        </div>
+
+                    </div>
+                </div>
+                <Grid columns={2} divided>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <div className="brief-info">
+                                <h2 className="">Music Listeners</h2>
+                                <p className="text-muted text-md m-b-lg">Help break your favorite bands</p>
+                                <p className=""> Remember when Kings Of Leon was cool? Remember when they looked like Porny Southern Mechanics? So do we. What if every artist you got in on before they broke big paid you a share of their success?         I mean you invested your ears. Now invest your mine.</p>
+
+                                <Header as='h3' icon>
                                     <Icon name='sound' />
                                     Sign up as an
                                         <Header.Subheader>
@@ -71,73 +76,28 @@ export default class Home extends Component {
                                     </Header.Subheader>
                                 </Header>
                             </div>
-                            {/* <svg id="logoAnimation" width="100" height="100" viewBox="0 0 300 300">
-                                <defs>
-                                    <linearGradient id="gradient-fill" gradientUnits="userSpaceOnUse"
-                                        x1="0" y1="300" x2="300" y2="0">
-                                        <stop offset="0%">
-                                            <animate attributeName="stop-color" values="#00E06B;#CB0255;#00E06B" dur="5s" repeatCount="indefinite" />
-                                        </stop>
-                                        <stop offset="100%">
-                                            <animate attributeName="stop-color" values="#04AFC8;#8904C5;#04AFC8" dur="8s" repeatCount="indefinite" />
-                                        </stop>
-                                    </linearGradient>
-                                    <clipPath id="clip">
-                                        <rect className="square s1" x="0" y="0" rx="12" ry="12" height="90" width="90"></rect>
-                                        <rect className="square s2" x="100" y="0" rx="12" ry="12" height="90" width="90"></rect>
-                                        <rect className="square s3" x="200" y="0" rx="12" ry="12" height="90" width="90"></rect>
-                                        <rect className="square s4" x="0" y="100" rx="12" ry="12" height="90" width="90"></rect>
-                                        <rect className="square s5" x="200" y="100" rx="12" ry="12" height="90" width="90"></rect>
-                                        <rect className="square s6" x="0" y="200" rx="12" ry="12" height="90" width="90"></rect>
-                                        <rect className="square s7" x="100" y="200" rx="12" ry="12" height="90" width="90"></rect>
-                                    </clipPath>
-                                </defs>
-                                <rect className="gradient" clip-path="url('#clip')" height="300" width="300"></rect>
-                            </svg> */}
 
-
-
-
-
-                        
-                    </div>
-                </div>
-
-
-                <div className="row-col">
-                    <div className="col-sm-6">
-                        <div className="black cover cover-gd brief" style={{ backgroundImage: 'url(/images/b1.jpg)' }}>
-                            <div className="p-a-lg text-center">
-                                <h3 className="">Music Listeners</h3>
-                                <p className="text-muted text-md m-b-lg">Help break your favorite bands</p>
-                                <Link to='/artists/signup' className="btn circle white m-b-lg p-x-md">Sign Up</Link>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div className="brief-info">
+                                <h2 className="">Music Makers</h2>
+                                <p className="text-muted text-md m-b-lg">You're getting paid again</p>
+                                <p>BlockParty is the first music platform that can honestly say it puts the Artist first.</p>
+                                <Header as='h3' icon>
+                                    <Icon name='music' />
+                                    Sign up as an
+                                        <Header.Subheader color='white'>
+                                        <Link to='/artists/signup'>Artists</Link>
+                                    </Header.Subheader>
+                                </Header>
                             </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-6 black lt">
-                        <div className="black cover cover-gd brief" style={{ backgroundImage: 'url(/images/b7.jpg)' }}>
-                            <div className="p-a-lg text-center">
-                                <h3 className="">Music Makers</h3>
-                                <p className="text-muted text-md m-b-lg">Artists are getting paid again</p>
-                                <Link to='/users/signup' className="btn circle white m-b-lg p-x-md">Sign Up</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row-col dark-white">
-                    <div className="col-md-3"></div>
-                    <div className="col-md-6">
-                        <div className="p-a-lg text-center">
-                            <h3 className="display-4 m-y-lg">BlockParty is the first music platform that can honestly say it puts the Artist first.</h3>
-                            <p className="text-muted text-md m-b-lg">With the advent of Blockchain technology we've created a platform where every second you listen to your favorite artist you're mining on the block. Here's what's incredible: It pays you and your artist. Remember when Kings Of Leon was cool? Remember when they looked like Porny Southern Mechanics? So do we. What if every artist you got in on before they broke big paid you a share of their success?         I mean you invested your ears. Now invest your mine.</p>
-                            <a className="btn circle btn-outline b-black m-b-lg p-x-md">Learn More</a>
-                        </div>
-                    </div>
-                    <div className="col-md-3"></div>
-                </div>
+                        </Grid.Column>
+                    </Grid.Row>
 
+                </Grid>
+ 
 
-                <Segment inverted vertical style={{ padding: '5em 0em' }}>
+                <Segment inverted vertical style={{ padding: '5em 0em' }} id="footer">
                     <Container>
                         <Grid divided inverted stackable>
                             <Grid.Row>
